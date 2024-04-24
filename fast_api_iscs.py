7983e2b4383f6a54eddbf8da84c3020db201edd9
salvapathi <salvapathinaidu@gmail.com>
2024-04-24 18:43:01 +0530

    the 8th commit

--- fast_api_iscs.py
+++ fast_api_iscs.py
@@ -1,8 +1,10 @@
 import mysql.connector
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import uvicorn
 import os
+from datetime import datetime
+import pytz
 
 # Create an instance of FastAPI
 app = FastAPI(title="REST API using FastAPI mysql server iscs data")
@@ -10,7 +12,9 @@ hostname = os.environ.get('DB_HOSTNAME')
 username = os.environ.get('DB_USERNAME')
 password = os.environ.get('DB_PASSWORD')
 database = os.environ.get('DB_NAME')
-# Establish connection to the database
+#Establish connection to the database
+# # Define database credentials
+
 
 connection = mysql.connector.connect(
     host=hostname,
@@ -25,7 +29,13 @@ if connection.is_connected():
 
     # Define a route to execute the SELECT query
     @app.get("/recruitment/")
-    def get_recruitment_data(technology: str):
+    def get_recruitment_data(user_name: str,technology: str,request: Request): #
+       
+        
+
+       
+        # Get the client's IP address
+        client_ip = request.client.host
         # Create a cursor object
         cursor = connection.cursor()
 
@@ -40,10 +50,18 @@ if connection.is_connected():
         
         # Create a DataFrame
         df = pd.DataFrame(rows, columns=columns)
+        #timestap
+        ist = pytz.timezone('Asia/Kolkata')  # Indian Standard Time
+        ist_time = datetime.now(ist)
+
+        # Convert IST time to MySQL datetime format (YYYY-MM-DD HH:MM:SS)
+        ist_time_str = ist_time.strftime('%Y-%m-%d %H:%M:%S')
+        time_stamp_sql="INSERT INTO customer_login_details (user_name,api_hit_time,ip_address) VALUE (%s,%s,%s)"
+        cursor.execute(time_stamp_sql, (user_name,ist_time_str,client_ip))
+        connection.commit()
   
         
-        # # Return the DataFrame
-        return df.iloc[:, :10].to_dict(orient="records")
+        return df.iloc[:, :10].to_dict(orient="records") 
         
 
     # Close the connection when the application stops
